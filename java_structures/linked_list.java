48463d6b907c5b17d4111bbfed39a626fe010b4a
GBell <gunnarbell91@gmail.com>
2011-12-24 20:16:34 -0500

    commit

--- java_structures/linked_list.java
+++ java_structures/linked_list.java
@@ -9,7 +9,7 @@ public class linked_list {
     }
 
     public int insert(list_node head, String key_in, int data_in) {
-	if (contains(head, key_in) == 0) {
+	if (contains(head, key_in, data_in) == 0) {
 	    return 0;
 	    // contains will do the updating work
 	} else {
@@ -19,9 +19,13 @@ public class linked_list {
 	    to_add.data = data_in;
 
 	    if (curr == null) {
-		curr = temp;
+		head = to_add;
 		to_add.next = null;
 	    } else {
+		if (strcmp(head.key, key_in) >= 0) {
+		    head = to_add;
+		    to_add.next = curr;
+		}
 		list_node prev = curr;
 		curr = curr.next;
 		while (curr != null) {
@@ -35,10 +39,49 @@ public class linked_list {
 	    } 
 	}
     }
+
+    public int size(list_node head) {
+	int ret_size = 0;
+	list_node curr = head;
+	while (curr != null) {
+	    curr = curr.next;
+	    ret_size++;
+	}
+	return ret_size;
+    }
+
+    public static void print_list(list_node head) {
+	list_node curr = head;
+	while (curr != null) {
+	    int cnt = 0;
+	    System.out.println("Node " + cnt);
+	    System.out.print("Key: " + curr.key + " ");
+	    System.out.println("Data: " + curr.data);
+	    curr = curr.next;
+	}
+	System.out.println("EOL");
+    }
+    
+    public static int contains(list_node head_in, String key_in, int data_in) {
+	list_node curr = head_in;
+	while (curr != null) {
+	    if (strcmp(curr.key, key) == 0) {
+		curr.data = data_in;
+		return 0;
+	    }
+	}
+    }
     
     private class list_node {
 	String key;
 	int data;
 	list_node next;
+    }
+
+    public static void main(String[] args) {
+	linked_list test = new linked_list();
+	print_list(test);
+	list.insert(list.head, "Apple", 6);
+	print_list(test);
     }
 }
--- java_structures/list_tests.java
+++ java_structures/list_tests.java
@@ -1,6 +1,27 @@
-public class list_tests extends TestCase () {
+import linked_list.java;
+import org.junit;
+import java.lang.string;
+
+public class list_tests extends TestCase {
 
     public void test_insert() {
 	
+    }
+
+    public static void print_list(list_node head) {
+	list_node curr = head;
+	while (curr != null) {
+	    int cnt = 0;
+	    System.out.println("Node " + cnt);
+	    System.out.print("Key: " + curr.key + " ");
+	    System.out.println("Data: " + curr.data);
+	    curr = curr.next;
+	}
+    }
+
+    public static void main(String[] args) {
+	linked_list test = new linked_list();
+	
+	list.insert(list.head, "Apple", 6);
     }
 }
